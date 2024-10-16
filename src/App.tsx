9c24174686bea00c069af6c60a5a5d0f782d2677
Tausif <tausif00100@gmail.com>
2024-10-16 13:42:09 +0530

    Added Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div >
+      <p className='  text-green-500  text-8xl'>App </p>
+      
+      </div>
+  )
+}
+
+export default App
